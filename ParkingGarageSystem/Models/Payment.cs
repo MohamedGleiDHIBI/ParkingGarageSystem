cff45b459379bbe8695a25d4576615d8cd477cbd
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-19 15:11:21 +0100

    Add Class Payment

---
+++ ParkingGarageSystem/Models/Payment.cs
@@ -0,0 +1,14 @@
+﻿namespace ParkingGarageSystem.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int ReservationId { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentStatus { get; set; }
+        public string TransactionId { get; set; }
+        public int UserId { get; set; }
+    }
+}
