416078fddb43b18a4d7c71364b9f1fae6b92e2bb
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-19 15:14:07 +0100

    Add Class Location

---
+++ ParkingGarageSystem/Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingGarageSystem.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
