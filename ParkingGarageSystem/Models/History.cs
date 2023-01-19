b5d6621ed68f31cf293ced87e53039a261d39709
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-19 15:16:29 +0100

    Add Class History

---
+++ ParkingGarageSystem/Models/History.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingGarageSystem.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int LocationId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
