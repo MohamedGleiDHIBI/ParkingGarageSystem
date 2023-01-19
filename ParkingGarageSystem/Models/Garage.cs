439316f2c3e9869095f1e93dc430bb3e8c2197a8
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-19 15:05:53 +0100

    add model Garage

---
+++ ParkingGarageSystem/Models/Garage.cs
@@ -0,0 +1,16 @@
+﻿namespace ParkingGarageSystem.Models
+{
+    public class Garage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TotalSpots { get; set; }
+        public int AvailableSpots { get; set; }
+        public string Address { get; set; }
+        public string OperatingHours { get; set; }
+        public bool Security { get; set; }
+        public bool Accessibility { get; set; }
+        public string Photos { get; set; }
+        public string Description { get; set; }
+    }
+}
