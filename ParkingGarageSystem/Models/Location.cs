d8f837474eccb17568d188c51454d292ff4f9032
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-19 15:30:55 +0100

    Add Dependencies To Location Class

--- ParkingGarageSystem/Models/Location.cs
+++ ParkingGarageSystem/Models/Location.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsAvailable { get; set; }
+        public int GarageId { get; set; }
+        public virtual Garage Garage { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
     }
 }
