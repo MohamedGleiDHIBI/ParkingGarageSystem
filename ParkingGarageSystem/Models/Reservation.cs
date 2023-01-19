31243d2739b510357bace16204c300070aa8e1de
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-19 15:00:57 +0100

    add class Reservation

---
+++ ParkingGarageSystem/Models/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingGarageSystem.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int LocationId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public DateTime CancellationDate { get; set; }
+        public string CancellationReason { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
