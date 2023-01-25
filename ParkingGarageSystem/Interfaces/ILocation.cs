8edb237625b65c1768eb904ae328c38d340a0cda
mgdhibi@integrationobjects.com <Life12358(hamma)>
2023-01-25 11:24:04 +0100

    Implement Location Controller

---
+++ ParkingGarageSystem/Interfaces/ILocation.cs
@@ -0,0 +1,13 @@
+﻿using ParkingGarageSystem.Models;
+
+namespace ParkingGarageSystem.Interfaces
+{
+    public interface ILocation
+    {
+        Task<IEnumerable<Location>> GetAllLocations();
+        Task<Location> GetLocationById(int id);
+        Task CreateLocation(Location location);
+        Task UpdateLocation(Location location);
+        Task DeleteLocation(int id);
+    }
+}
